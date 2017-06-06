8f2811b9fc5ffb55fbdd099070becc7e144c64dd
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-06-05 18:05:28 -0700

    added hero service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by mark on 6/5/17.
+ */
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+}
